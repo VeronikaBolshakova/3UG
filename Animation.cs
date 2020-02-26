048947ba643fe9b5fb83cd9cb3162252d683d2d4
Veronika <veronika.bolshakova@enti.cat>
2020-02-26 12:46:34 +0100

    Animation

---
+++ Animation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation : MonoBehaviour
+{
+    public Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("miau");
+        // Agafem input (com ja vau veure)
+        bool walk = Input.GetButton("Horizontal");
+        bool stop = Input.GetButtonDown("Fire2");
+        bool shoot = Input.GetButtonDown("Fire3");
+        bool jump = Input.GetButtonDown("Jump");
+        // Activem triggers segons input previ
+        //if (walk)
+        //    animator.SetTrigger("Walk");
+        animator.SetBool("walk", walk);
+        //if (stop)
+            //animator.SetTrigger("Stop");
+        animator.SetBool("stop", stop);
+        animator.SetBool("shoot", shoot);
+        animator.SetBool("jump", jump);
+    }
+}
