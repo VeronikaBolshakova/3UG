33f54bbfe082a01a3e345655e9e88a8799245ee6
Joan <joan.mesalles@enti.cat>
2020-05-11 20:21:26 +0200

    Pause Menu + Soundtrack

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -6,20 +6,43 @@ public class AudioManager : MonoBehaviour
 {
     public Sound[] sounds;
 
+    public static AudioManager instance;
+
     void Awake()
     {
-        foreach(Sound s in sounds)
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach (Sound s in sounds)
         {
             s.source = gameObject.AddComponent<AudioSource>();
             s.source.clip = s.clip;
             s.source.volume = s.volume;
+            s.source.loop = s.loop;
         }
+
+    }
+
+    void Start()
+    {
+        Play("Theme");
 
     }
 
     public void Play (string name)
     {
-       Sound s = Array.Find(sounds, sounds => sounds.name == name);
+       Sound s = Array.Find(sounds, sound => sound.name == name);
+        
+
         s.source.Play();
     }
 
