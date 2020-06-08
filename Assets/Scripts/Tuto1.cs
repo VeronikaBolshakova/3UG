f00335af008bac397099c13410ff50bfe9274a34
Joan <joan.mesalles@enti.cat>
2020-06-08 22:14:26 +0200

    Tuto 1

---
+++ Assets/Scripts/Tuto1.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Tuto1 : MonoBehaviour
+{
+    public GameObject player;
+    public TextMeshProUGUI text_Tuto;
+    private float transparency = 1f;
+    void Update()
+    {
+        if(player.transform.position.x >= -10) { 
+            text_Tuto.alpha = transparency;
+            transparency -= 0.1f;
+        }
+
+
+
+    }
+}
